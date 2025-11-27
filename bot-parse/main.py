7404a985878e796cffe6b6a2e9097426fadcae83
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-11-27 13:02:02 +0300

    init start bot

---
+++ bot-parse/main.py
@@ -0,0 +1,12 @@
+import asyncio
+
+from bot import bot_settings, dp
+
+
+async def main():
+    await bot_settings.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot_settings)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
